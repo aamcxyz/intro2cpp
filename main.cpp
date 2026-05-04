bfa39c5e3b410a07be8497942a0b462a7e4fad4b
akua <akua@sent.com>
2026-05-04 02:17:27 -0400

    new modifications

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 int main() {
-    std:: cout << "Hello world! My name is Akua McEachron";
+    std:: cout << "Hello world! My name is Akua McEachron\n";
+    std:: cout << "I'm excited for whats's to come with learning C++ code.\n";
+    std:: cout << "Whose to say that I might be the next person to write rocket code!\n";
 
     return 0;
 }
